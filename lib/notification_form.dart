247c4fd53d6afc5d705c376d298101080af067a7
Alok Singh <alokrpt@gmail.com>
2023-12-25 22:14:45 +0530

    first commit

---
+++ lib/notification_form.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotificationForm extends StatelessWidget {
+  const NotificationForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
